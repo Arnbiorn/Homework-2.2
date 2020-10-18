f480e51e37eb173ea9c07916d273eeb764704d69
Arnbiorn <skrutelev988@gmail.com>
2020-10-18 19:58:02 +0300

    Configuring the app, implementing color changes in the view

--- Homework 2.2/ViewController.swift
+++ Homework 2.2/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorChanges()
+        changeLabelValues()
     }
-
-
+    
+    func colorChanges() {
+        coloredView.backgroundColor = .init(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    func changeLabelValues() {
+        redValueLabel.text = String(round(100 * Double(redSlider.value)) / 100)
+        greenValueLabel.text = String(round(100 * Double(greenSlider.value)) / 100)
+        blueValueLabel.text = String(round(100 * Double(blueSlider.value)) / 100)
+    }
+    
+    @IBAction func recolorSlider() {
+        colorChanges()
+        changeLabelValues()
+    }
+    
+    
 }
 
